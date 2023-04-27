b19377c289c28b4b177a53abad8229f2121fd071
PIYUSH <piyush.tripathi@gameium.com>
2023-04-27 10:40:11 +0530

    commit to push

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@Getter
+@Setter
+@Document(collection = "employee")
+
+public class Employee {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private String city;
+
+    private String phone;
+
+    private String project;
+
+}
